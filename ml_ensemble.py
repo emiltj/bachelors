f8fe59cf9bc6fffc6f534f9d5eb25bea8f845c0f
Emil_Jessen <emil.tj@hotmail.com>
2020-11-24 18:38:48 +0100

    fixed scripts + new folders

--- ml_ensemble.py
+++ ml_ensemble.py
@@ -4,9 +4,222 @@ from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib as plt
 import pandas as pd
 import numpy as np
-import os
 
-os.getcwd()
+data1 = [pd.read_csv('./csv_files/train1.csv', index_col=0), 1, pd.read_csv('./csv_files/holdout1.csv')]
+data2 = [pd.read_csv('./csv_files/train2.csv', index_col=0), 2, pd.read_csv('./csv_files/holdout2.csv')]
+data3 = [pd.read_csv('./csv_files/train3.csv', index_col=0), 3, pd.read_csv('./csv_files/holdout3.csv')]
+data4 = [pd.read_csv('./csv_files/train4.csv', index_col=0), 4, pd.read_csv('./csv_files/holdout4.csv')]
+data5 = [pd.read_csv('./csv_files/train5.csv', index_col=0), 5, pd.read_csv('./csv_files/holdout5.csv')]
+
+################## Creating a loop, so I won't have to do the below code 5 times ##################
+# Making the datasets into a list
+datasets = [data1, data2, data3, data4, data5]
+
+#creating empty objects to save data in
+classif_reports = ["", "", "", "", ""]
+conf_mtxs = []
+model = SVC(kernel = 'linear', class_weight = 'balanced')
+SVM_coef = pd.DataFrame(columns = ['predictor_name', 'coef', 'fold'])
+
+#defining which feature set is currently run 
+feature_set = 'holdout_performance'
+
+for train, n, holdout in datasets:   
+    # Dividing train and holdout up into predictor variables, and Diagnosis
+    x = train.iloc[:,4:]
+    y = train.loc[:,['ID', 'Diagnosis']]
+    y = y.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
+
+    x_holdout = holdout.iloc[:,4:]
+    y_holdout = holdout.loc[:,['ID', 'Diagnosis']]
+    y_holdout = y_holdout.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
+
+    # Fit the data onto the model
+    model.fit(x, y)
+
+    # Predict the holdout set (on the basis of predictor variables), using the fitted model
+    y_predictions = model.predict(x_holdout)
+
+    print(y_predictions)
+
+    # Get out the coefficients
+    coefs = model.coef_[0]
+    
+    # Get out the coefficient names
+    coefs_names = list(x_holdout)
+    coefs_names = np.asarray(coefs_names)
+
+    # Create an array with info on which folds data/features we're handling!
+    fold = np.repeat(n, len(coefs))
+
+    # Load in the predictor names, and their coefficients to the empty dataframe
+    SVM_coef = SVM_coef.append(pd.DataFrame({'predictor_name': coefs_names, 'coef' : coefs, 'fold' : fold}), ignore_index = True)
+
+    # Get classification report
+    report = classification_report(y_holdout, y_predictions, output_dict = True)
+    report_number = n-1
+    classif_reports[report_number] = report
+    
+    # Get confusion matrix
+    matrixx = confusion_matrix(y_holdout, y_predictions)
+    conf_mtxs.append(matrixx)
+    classif_reports[4]
+
+    #Saving data
+   
+    # making variables to use in filenames
+    svm_coef_name = "".join(['./performance/holdout/', str(feature_set), "_coef_holdout_fold", ".csv"]) 
+    conf_matrix_name = "".join(['./performance/holdout/', str(feature_set),"_confusion_matrix", str(n), ".csv"]) 
+    classification_report_name = "".join(['./performance/holdout/', str(feature_set),"_classification_report", str(n), ".csv"]) 
+
+
+    # Loading each element (classification_report) of the list of dictionaries, as a dataframe 
+    classif_report_fold = pd.DataFrame(classif_reports[n-1])
+    # Loading each element (classification_report) of the list of dictionaries, as a dataframe
+    conf_matrix_fold = pd.DataFrame(conf_mtxs[n-1])
+    #Give the matrices names for rows and columns
+    conf_matrix_fold.columns = conf_matrix_fold.columns = ['predict_td', 'predict_sz']
+    conf_matrix_fold.index = conf_matrix_fold.index = ['true_td', 'true_sz']
+
+    # Writing coefficients to .csv
+    SVM_coef.to_csv(svm_coef_name, sep=',', index = True)
+    # Writing confusion matrices to csv
+    conf_matrix_fold.to_csv(conf_matrix_name, sep=',', index = True)
+    # Writing classification reports to csv
+    classif_report_fold.to_csv(classification_report_name, sep=',', index = True)
+
+    # The end
+
+# Testing that the labels for the confusion matrices fit -> Yes they do!!!
+# datasets[0][2].loc[datasets[0][2]['Diagnosis'] == 0]
+# datasets[0][2].loc[datasets[0][2]['Diagnosis'] == 1]
+# pd.read_csv("performance_measures_5_lassos/performance_measures_5_feature_sets_ConfusionMatrix1.csv")
+# pd.read_csv("performance_measures_5_lassos/performance_measures_5_feature_sets_ClassificationReport1.csv")
+
+pd.read_csv("performance/holdout/holdout_performance_classification_report1.csv")
+pd.read_csv("performance/holdout/holdout_performance_classification_report2.csv")
+pd.read_csv("performance/holdout/holdout_performance_classification_report3.csv")
+pd.read_csv("performance/holdout/holdout_performance_classification_report4.csv")
+pd.read_csv("performance/holdout/holdout_performance_classification_report5.csv")
+
+pd.read_csv("performance/holdout/holdout_performance_confusion_matrix1.csv")
+pd.read_csv("performance/holdout/holdout_performance_confusion_matrix2.csv")
+pd.read_csv("performance/holdout/holdout_performance_confusion_matrix3.csv")
+pd.read_csv("performance/holdout/holdout_performance_confusion_matrix4.csv")
+pd.read_csv("performance/holdout/holdout_performance_confusion_matrix5.csv")
+
+pd.read_csv("performance/holdout/holdout_performance_coef_holdout_fold.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Old function with shitty results (but worked)
+import sklearn as sk
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib as plt
+import pandas as pd
+import numpy as np
+import os
 
 # Load train and test data
 data1 = [pd.read_csv('./csv_files/train1.csv', index_col=0), 1, pd.read_csv('./csv_files/holdout1.csv', index_col=0)]
@@ -15,11 +228,6 @@ data3 = [pd.read_csv('./csv_files/train3.csv', index_col=0), 3, pd.read_csv('./c
 data4 = [pd.read_csv('./csv_files/train4.csv', index_col=0), 4, pd.read_csv('./csv_files/holdout4.csv', index_col=0)]
 data5 = [pd.read_csv('./csv_files/train5.csv', index_col=0), 5, pd.read_csv('./csv_files/holdout5.csv', index_col=0)]
 
-data1[0]
-data2[0]
-data1[2]
-data2[2]
-
 # Take the lists and include them in a list
 datasets = [data1, data2, data3, data4, data5]
 
@@ -27,7 +235,7 @@ datasets = [data1, data2, data3, data4, data5]
 classif_reports = ["", "", "", "", ""]
 
 # Defining the model we're using
-model = SVC(kernel = 'linear')
+model = SVC(kernel = 'linear', class_weight = 'balanced')
 
 # Preparing a dataframe for the predictions
 predictions = pd.DataFrame({'ID' : datasets[0][2].loc[:,'ID'], 'sex' : datasets[0][2].loc[:, 'Gender'] , 'diagnosis_real' : datasets[0][2].loc[:,'Diagnosis']})
@@ -39,7 +247,6 @@ for train, k, holdout in datasets:
     # making td into 0 and sz into 1 for both train and holdout
     train['Diagnosis'] = train['Diagnosis'].apply(lambda x: 0 if x=='td' else 1) # setting td as 0 and ASD as 1
     holdout['Diagnosis'] = holdout['Diagnosis'].apply(lambda x: 0 if x=='td' else 1) # setting td as 0 and ASD as 1
-
 
     # Cutting my training data up into x and y (so the model can be fit)
     x = train.iloc[:,4:]
@@ -61,7 +268,7 @@ for train, k, holdout in datasets:
     print(y_predicted)
     
     # To test how they predict!!
-    report = classification_report(y_holdout, y_predicted)#, output_dict = True)
+    report = pd.DataFrame(classification_report(y_holdout, y_predicted, output_dict = True))
     report_number = k-1
     classif_reports[report_number] = report
     
@@ -77,18 +284,15 @@ for train, k, holdout in datasets:
     # Add a column to the dataframe (with unique name, and the predictions)
     predictions[new_col_name] = y_predicted
 
-# For testing!!
-print(classif_reports[1])
-
 # For each row, get a count of 1's and 0's in the new columns
 count_of_1_predictions = predictions.iloc[:,-5:].apply(pd.Series.value_counts, axis=1)[1].fillna(0)
 predictions['diagnosis_predic_ensemble'] = [0 if x < 3 else 1 for x in count_of_1_predictions]
 
-predictions
-
 # Performance of ensemble_model on all participants
 classification_report_ensemble = pd.DataFrame(classification_report(predictions['diagnosis_real'], predictions['diagnosis_predic_ensemble'], output_dict = True))
 conf_matrix_ensemble = pd.DataFrame(confusion_matrix(predictions['diagnosis_real'], predictions['diagnosis_predic_ensemble']))
+classification_report_ensemble
+conf_matrix_ensemble
 
 # Performance of ensemble_model on women
 predictions_female = predictions[predictions['sex'] == 'F']
@@ -107,6 +311,13 @@ conf_matrix_ensemble_male = pd.DataFrame(confusion_matrix(predictions_male['diag
 # All predictions
 predictions
 
+# Performance of invididual submodels
+classif_reports[0]
+classif_reports[1]
+classif_reports[2]
+classif_reports[3]
+classif_reports[4]
+
 # Performance of ensemble
 classification_report_ensemble
 conf_matrix_ensemble
@@ -118,101 +329,4 @@ conf_matrix_ensemble_female
 #Performance of ensemble for male
 classification_report_ensemble_male
 conf_matrix_ensemble_male
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-################## Creating a loop, so I won't have to do the below code 5 times ##################
-
-#creating empty objects to save data in
-classif_reports = ["", "", "", "", ""]
-conf_mtxs = []
-model = SVC(kernel = 'linear')
-SVM_coef = pd.DataFrame(columns = ['predictor_name', 'coef', 'fold'])
-
-#defining which feature set is currently run 
-feature_set = 'performance_measures_5_feature_sets'
-
-for df, n in datasets:
-    # making td into 0 and sz into 1
-    df['Diagnosis'] = df['Diagnosis'].apply(lambda x: 0 if x=='td' else 1) # setting TD as 0 and ASD as 1
-   
-    # Divide the data up into train and test
-    df_test = df.loc[df['fold'] == n]
-    df_train = df.loc[df['fold'] != n]
-
-    # Dividing train and test up into predictor variables, and Diagnosis
-    x_train1 = df_train.iloc[:,4:]
-    y_train1 = df_train.loc[:,['ID', 'Diagnosis']]
-    y_train1 = y_train1.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
-
-    x_test1 = df_test.iloc[:,4:]
-    y_test1 = df_test.loc[:,['ID', 'Diagnosis']]
-    y_test1 = y_test1.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
-
-    # Fit the data onto the model
-    model.fit(x_train1, y_train1)
-
-    # Predict the test set (on the basis of predictor variables), using the fitted model
-    predictions_test1 = model.predict(x_test1)
-
-    # Get out the coefficients
-    coef1 = model.coef_
-    coef1 = coef1[0]
-
-    # Get out the coefficient names
-    coef1_names = list(x_test1)
-    coef1_names = np.asarray(coef1_names)
-
-    # Create an array with info on which folds data/features we're handling!
-    fold = np.repeat(n, len(coef1))
-
-    # Load in the predictor names, and their coefficients to the empty dataframe
-    SVM_coef = SVM_coef.append(pd.DataFrame({'predictor_name': coef1_names, 'coef' : coef1, 'fold' : fold}), ignore_index = True)
-
-    # Get classification report
-    report = classification_report(y_test1, predictions_test1, output_dict = True)
-    report_number = n-1
-    classif_reports[report_number] = report
-    
-    # Get confusion matrix
-    matrixx = confusion_matrix(y_test1, predictions_test1)
-    conf_mtxs.append(matrixx)
-
-    #Saving data 
-   
-    #making variables to use in filenames
-    svm_coef_name = "".join(['./performance_measures_5_lassos/', str(feature_set), "_CoefTestFold", ".csv"]) 
-    conf_matrix_name = "".join(['./performance_measures_5_lassos/', str(feature_set),"_ConfusionMatrix", str(n), ".csv"]) 
-    classification_report_name = "".join(['./performance_measures_5_lassos/', str(feature_set),"_ClassificationReport", str(n), ".csv"]) 
-
-
-    # Loading each element (classification_report) of the list of dictionaries, as a dataframe 
-    classif_report_fold = pd.DataFrame(classif_reports[n-1])
-    # Loading each element (classification_report) of the list of dictionaries, as a dataframe
-    conf_matrix_fold = pd.DataFrame(conf_mtxs[n-1])
-    #Give the matrices names for rows and columns
-    conf_matrix_fold.columns = conf_matrix_fold.columns = ['predict_asd', 'predict_td']
-    conf_matrix_fold.index = conf_matrix_fold.index = ['actual_asd', 'actual_td']
-
-    # Writing coefficients to .csv
-    SVM_coef.to_csv(svm_coef_name, sep=',', index = True)
-    # Writing confusion matrices to csv
-    conf_matrix_fold.to_csv(conf_matrix_name, sep=',', index = True)
-    # Writing classification reports to csv
-    classif_report_fold.to_csv(classification_report_name, sep=',', index = True)
-
-
-    # The end
-
 
--- performance_measures_5_lassos.py
+++ performance_measures_5_lassos.py
@@ -5,78 +5,72 @@ import matplotlib as plt
 import pandas as pd
 import numpy as np
 
-
-train1 = [pd.read_csv('./csv_files/train1.csv', index_col=0),1]
-train2 = [pd.read_csv('./csv_files/train2.csv', index_col=0),2]
-train3 = [pd.read_csv('./csv_files/train3.csv', index_col=0),3]
-train4 = [pd.read_csv('./csv_files/train4.csv', index_col=0),4]
-train5 = [pd.read_csv('./csv_files/train5.csv', index_col=0),5]
+data1 = [pd.read_csv('./csv_files/train1.csv', index_col=0), 1, pd.read_csv('./csv_files/test1.csv')]
+data2 = [pd.read_csv('./csv_files/train2.csv', index_col=0), 2, pd.read_csv('./csv_files/test2.csv')]
+data3 = [pd.read_csv('./csv_files/train3.csv', index_col=0), 3, pd.read_csv('./csv_files/test3.csv')]
+data4 = [pd.read_csv('./csv_files/train4.csv', index_col=0), 4, pd.read_csv('./csv_files/test4.csv')]
+data5 = [pd.read_csv('./csv_files/train5.csv', index_col=0), 5, pd.read_csv('./csv_files/test5.csv')]
 
 ################## Creating a loop, so I won't have to do the below code 5 times ##################
 # Making the datasets into a list
-datasets = [train1, train2, train3, train4, train5]
+datasets = [data1, data2, data3, data4, data5]
 
 #creating empty objects to save data in
 classif_reports = ["", "", "", "", ""]
 conf_mtxs = []
-model = SVC(kernel = 'linear')
+model = SVC(kernel = 'linear', class_weight = 'balanced')
 SVM_coef = pd.DataFrame(columns = ['predictor_name', 'coef', 'fold'])
 
 #defining which feature set is currently run 
-feature_set = 'performance_measures_5_feature_sets'
+feature_set = 'test_performance'
 
-for df, n in datasets:
-    # making td into 0 and sz into 1
-    df['Diagnosis'] = df['Diagnosis'].apply(lambda x: 0 if x=='td' else 1) # setting TD as 0 and ASD as 1
-   
-    # Divide the data up into train and test
-    df_test = df.loc[df['fold'] == n]
-    df_train = df.loc[df['fold'] != n]
-
+for train, n, test in datasets:   
     # Dividing train and test up into predictor variables, and Diagnosis
-    x_train1 = df_train.iloc[:,4:]
-    y_train1 = df_train.loc[:,['ID', 'Diagnosis']]
-    y_train1 = y_train1.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
+    x = train.iloc[:,4:]
+    y = train.loc[:,['ID', 'Diagnosis']]
+    y = y.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
 
-    x_test1 = df_test.iloc[:,4:]
-    y_test1 = df_test.loc[:,['ID', 'Diagnosis']]
-    y_test1 = y_test1.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
+    x_test = test.iloc[:,5:]
+    y_test = test.loc[:,['ID', 'Diagnosis']]
+    y_test = y_test.set_index('ID') # Setting index as ID, to be able to map how well the model predicts on genders
 
     # Fit the data onto the model
-    model.fit(x_train1, y_train1)
+    model.fit(x, y)
 
     # Predict the test set (on the basis of predictor variables), using the fitted model
-    predictions_test1 = model.predict(x_test1)
+    y_predictions = model.predict(x_test)
+
+    print(y_predictions)
 
     # Get out the coefficients
-    coef1 = model.coef_
-    coef1 = coef1[0]
-
+    coefs = model.coef_[0]
+    
     # Get out the coefficient names
-    coef1_names = list(x_test1)
-    coef1_names = np.asarray(coef1_names)
+    coefs_names = list(x_test)
+    coefs_names = np.asarray(coefs_names)
 
     # Create an array with info on which folds data/features we're handling!
-    fold = np.repeat(n, len(coef1))
+    fold = np.repeat(n, len(coefs))
 
     # Load in the predictor names, and their coefficients to the empty dataframe
-    SVM_coef = SVM_coef.append(pd.DataFrame({'predictor_name': coef1_names, 'coef' : coef1, 'fold' : fold}), ignore_index = True)
+    SVM_coef = SVM_coef.append(pd.DataFrame({'predictor_name': coefs_names, 'coef' : coefs, 'fold' : fold}), ignore_index = True)
 
     # Get classification report
-    report = classification_report(y_test1, predictions_test1, output_dict = True)
+    report = classification_report(y_test, y_predictions, output_dict = True)
     report_number = n-1
     classif_reports[report_number] = report
     
     # Get confusion matrix
-    matrixx = confusion_matrix(y_test1, predictions_test1)
+    matrixx = confusion_matrix(y_test, y_predictions)
     conf_mtxs.append(matrixx)
+    classif_reports[4]
 
-    #Saving data 
+    #Saving data
    
-    #making variables to use in filenames
-    svm_coef_name = "".join(['./performance_measures_5_lassos/', str(feature_set), "_CoefTestFold", ".csv"]) 
-    conf_matrix_name = "".join(['./performance_measures_5_lassos/', str(feature_set),"_ConfusionMatrix", str(n), ".csv"]) 
-    classification_report_name = "".join(['./performance_measures_5_lassos/', str(feature_set),"_ClassificationReport", str(n), ".csv"]) 
+    # making variables to use in filenames
+    svm_coef_name = "".join(['./performance/test/', str(feature_set), "_coef_test_fold", ".csv"]) 
+    conf_matrix_name = "".join(['./performance/test/', str(feature_set),"_confusion_matrix", str(n), ".csv"]) 
+    classification_report_name = "".join(['./performance/test/', str(feature_set),"_classification_report", str(n), ".csv"]) 
 
 
     # Loading each element (classification_report) of the list of dictionaries, as a dataframe 
@@ -84,8 +78,8 @@ for df, n in datasets:
     # Loading each element (classification_report) of the list of dictionaries, as a dataframe
     conf_matrix_fold = pd.DataFrame(conf_mtxs[n-1])
     #Give the matrices names for rows and columns
-    conf_matrix_fold.columns = conf_matrix_fold.columns = ['predict_asd', 'predict_td']
-    conf_matrix_fold.index = conf_matrix_fold.index = ['actual_asd', 'actual_td']
+    conf_matrix_fold.columns = conf_matrix_fold.columns = ['predict_td', 'predict_sz']
+    conf_matrix_fold.index = conf_matrix_fold.index = ['true_td', 'true_sz']
 
     # Writing coefficients to .csv
     SVM_coef.to_csv(svm_coef_name, sep=',', index = True)
@@ -94,7 +88,25 @@ for df, n in datasets:
     # Writing classification reports to csv
     classif_report_fold.to_csv(classification_report_name, sep=',', index = True)
 
-
     # The end
 
+# Testing that the labels for the confusion matrices fit -> Yes they do!!!
+# datasets[0][2].loc[datasets[0][2]['Diagnosis'] == 0]
+# datasets[0][2].loc[datasets[0][2]['Diagnosis'] == 1]
+# pd.read_csv("performance_measures_5_lassos/performance_measures_5_feature_sets_ConfusionMatrix1.csv")
+# pd.read_csv("performance_measures_5_lassos/performance_measures_5_feature_sets_ClassificationReport1.csv")
+
+pd.read_csv("performance/test/test_performance_classification_report1.csv")
+pd.read_csv("performance/test/test_performance_classification_report2.csv")
+pd.read_csv("performance/test/test_performance_classification_report3.csv")
+pd.read_csv("performance/test/test_performance_classification_report4.csv")
+pd.read_csv("performance/test/test_performance_classification_report5.csv")
+
+pd.read_csv("performance/test/test_performance_confusion_matrix1.csv")
+pd.read_csv("performance/test/test_performance_confusion_matrix2.csv")
+pd.read_csv("performance/test/test_performance_confusion_matrix3.csv")
+pd.read_csv("performance/test/test_performance_confusion_matrix4.csv")
+pd.read_csv("performance/test/test_performance_confusion_matrix5.csv")
+
+pd.read_csv("performance/test/test_performance_coef_test_fold.csv")
 
